7c3266ddfbe4384a5d94f200e94ed0bd80572dfa
marina lyulko <marinalyulko@gmail.com>
2017-10-04 11:35:08 -0700

    basicCard and beginning of ClozeCard

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,11 +1,13 @@
 
 var ClozeCard = function(text, cloze) {
-	this.text = text.split(cloze);
+	this.fullText = text;
 	this.cloze = cloze;
+	this.partial = text.replace(cloze, "...")
+
 }
 
 var clozeCard1 = new ClozeCard("Albert Einstein won the noble prize for discovering the photoelectric effect in 1921", "Albert Einstein");
-var clozeCard2 = new ClozeCard("")
+var clozeCard2 = new ClozeCard("The Magna Carta was signed in 1215 by King John of England", "Magna Carta")
 
 
 module.exports = ClozeCard;
