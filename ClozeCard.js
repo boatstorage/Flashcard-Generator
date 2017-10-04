a6b1bace96a155839545da06e2a356759c4dd20a
marina lyulko <marinalyulko@gmail.com>
2017-10-04 13:46:50 -0700

    cloze card function works

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,12 +2,26 @@
 var ClozeCard = function(text, cloze) {
 	this.fullText = text;
 	this.cloze = cloze;
-	this.partial = text.replace(cloze, "...")
+	this.partial = partial(text, cloze);
+	 
+  function partial(text, cloze) {
+		if (text.includes(cloze)) {
 
+			
+	    return text.replace(cloze, "...");
+
+} else{
+  throw new Error("Whoops! this text does not contain those words");
 }
 
+}}
+
 var clozeCard1 = new ClozeCard("Albert Einstein won the noble prize for discovering the photoelectric effect in 1921", "Albert Einstein");
-var clozeCard2 = new ClozeCard("The Magna Carta was signed in 1215 by King John of England", "Magna Carta")
+var clozeCard2 = new ClozeCard("The Magna Carta was signed in 1215 by King John of England", "Magna Carta");
+
+console.log(clozeCard1.partial);
+
+
 
 
 module.exports = ClozeCard;
